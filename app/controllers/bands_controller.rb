55a7de4c822678c8636914c3756f3e0635b22c15
jimosk <jimosdk@gmail.com>
2021-08-07 17:32:40 +0300

    Created bands controller ,implemented actions index,show,new,create,edit,update,destroy along with corresponding views

---
+++ app/controllers/bands_controller.rb
@@ -0,0 +1,52 @@
+class BandsController < ApplicationController
+    def index 
+        render :index
+    end
+
+    def show
+        @band = Band.find_by(id: params[:id])
+        render :show
+    end
+
+    def new
+        @band = Band.new
+        render :new
+    end
+
+    def create
+        @band = Band.new(band_params)
+        if @band.save
+            redirect_to band_url(@band)
+        else  
+            flash.now[:errors] = @band.errors.full_messages
+            render :new
+        end
+    end
+
+    def edit
+        @band = Band.find_by(id: params[:id])
+        render :edit
+    end
+
+    def update 
+        @band = Band.find_by(id: params[:id])
+        if @band.update(band_params)
+            redirect_to band_url(@band)
+        else  
+            flash.now[:errors] = @band.errors.full_messages
+            render :edit
+        end
+    end
+
+    def destroy
+        @band = Band.find_by(id: params[:id])
+        @band.destroy
+        redirect_to bands_url
+    end
+
+    private 
+
+    def band_params
+        params.require(:band).permit(:name)
+    end
+end
