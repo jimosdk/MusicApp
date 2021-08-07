6a11d05af20264bd3e2a69c9cdc8cec0ae281c24
jimosk <jimosdk@gmail.com>
2021-08-07 17:04:55 +0300

    Created model User ,implemented corresponding validations and helper methods for auth pattern,also created model Band

---
+++ app/models/band.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: bands
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_bands_on_name  (name)
+#
+class Band < ApplicationRecord
+    validates :name,presence:true
+end
---
+++ app/models/user.rb
@@ -0,0 +1,53 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  email           :string           not null
+#  name            :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#
+# Indexes
+#
+#  index_users_on_email          (email) UNIQUE
+#  index_users_on_session_token  (session_token) UNIQUE
+#
+class User < ApplicationRecord
+
+    attr_reader :password
+
+    validates :session_token,:email,presence: true,uniqueness: true
+    validates :password_digest,presence:true
+    validates :name,presence:true
+    validates :password,length: {minimum:6,allow_nil:true}
+    after_initialize :ensure_session_token
+
+    def self.find_by_credentials(email,password)
+        user = User.find_by(email:email)
+        user && user.is_password?(password) ? user : nil
+    end
+
+    def self.generate_session_token
+        SecureRandom::urlsafe_base64(16)
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def ensure_session_token
+        self.session_token = User.generate_session_token
+    end
+
+    def reset_session_token!
+        self.session_token = User.generate_session_token
+        self.save!
+        self.session_token
+    end
+end
