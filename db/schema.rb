8c7047a99d041fc24d6b9a347bc54f1fe73cf556
jimosk <jimosdk@gmail.com>
2021-08-07 20:41:30 +0300

    Created table albums

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_07_103440) do
+ActiveRecord::Schema.define(version: 2021_08_07_143629) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "albums", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "year", null: false
+    t.boolean "live", default: false, null: false
+    t.integer "band_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["band_id"], name: "index_albums_on_band_id"
+  end
 
   create_table "bands", force: :cascade do |t|
     t.string "name", null: false
