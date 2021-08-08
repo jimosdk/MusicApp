1e8d28017a1024a51a70b7de4a8cc55f991fc7a2
jimosk <jimosdk@gmail.com>
2021-08-08 15:54:03 +0300

    Added nested resource :tracks to resource albums with action new and top level resource tracks with actions create,show,edit,update,destroy

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,9 @@ Rails.application.routes.draw do
     resources :albums,only: [:new]
   end
 
-  resources :albums,except: [:new,:index]
+  resources :albums,except: [:new,:index] do
+    resources :tracks,only: [:new]
+  end
+
+  resources :tracks,except: [:new,:index]
 end
