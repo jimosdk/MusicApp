a668fb8fe8530031dc7decfaa2b172102de69dab
jimosk <jimosdk@gmail.com>
2021-08-09 00:30:43 +0300

    Added collection route get :activate to resource users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :users ,only: [:create,:show,:new]
+  resources :users ,only: [:create,:show,:new] do
+    get :activate ,on: :collection
+  end
 
   resource :session ,only: [:create,:new,:destroy]
 
