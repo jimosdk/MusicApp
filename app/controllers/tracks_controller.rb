43fb352b3887f8bd8e304595cb659f40abe33473
jimosk <jimosdk@gmail.com>
2021-08-08 15:58:10 +0300

    Created Tracks Controller ,implemented actions new,create,update,edit,show,destroy and corresponding views and implemented partial view for new and edit

---
+++ app/controllers/tracks_controller.rb
@@ -0,0 +1,52 @@
+class TracksController < ApplicationController
+    before_action :require_current_user
+
+    def new
+        @track = Track.new
+        @track.album_id = params[:album_id]
+        render :new
+    end
+
+    def create
+        @track = Track.new(track_params)
+        if @track.save
+            redirect_to track_url(@track)
+        else  
+            flash.now[:errors] = @track.errors.full_messages
+            render :new
+        end
+    end
+
+    def show 
+        @track = Track.find_by(id: params[:id])
+        render :show
+    end
+
+    def edit
+        @track = Track.find_by(id: params[:id])
+        render :edit
+    end
+
+    def update  
+        @track = Track.find_by(id: params[:id])
+        if @track.update(track_params)
+            redirect_to track_url(@track)
+        else
+            flash.now[:errors] = @track.errors.full_messages 
+            render :edit 
+        end
+    end
+
+    def destroy  
+        @track = Track.find_by(id: params[:id])
+        album = @track.album 
+        @track.destroy 
+        redirect_to album_url(album)
+    end
+
+    private 
+
+    def track_params
+        params.require(:track).permit(:title,:ord,:lyrics,:album_id,:bonus)
+    end
+end
