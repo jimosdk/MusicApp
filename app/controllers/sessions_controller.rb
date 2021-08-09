4807b05d22dc7f96d17bccda32e2e84e76edb86e
jimosk <jimosdk@gmail.com>
2021-08-09 12:54:08 +0300

    Changed create action to redirect to bands index page after login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,7 @@ class SessionsController < ApplicationController
                                         params[:session][:password])
         if @user 
             log_in!
-            redirect_to user_url(@user)
+            redirect_to bands_url
         else   
             flash.now[:errors] = ['User not found']
             render :new
