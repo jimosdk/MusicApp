bf5252a7dc7931db50a213c64e6195bc94720e11
jimosk <jimosdk@gmail.com>
2021-08-09 12:52:29 +0300

    Implemented require_admin that will be used in callbacks for actions that require admin priviledges and implemented admin? method for checking if current user is an admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-    helper_method :current_user
+    helper_method :current_user,:admin?
 
     def current_user
         return nil unless session[:session_token]
@@ -26,9 +26,21 @@ class ApplicationController < ActionController::Base
     def require_activation
         @user = User.find_by_credentials(params[:session][:email],
                                         params[:session][:password])
-        unless @user.activated
+        unless @user 
+            flash.now[:errors] = ["User not found"]
+            render :new
+        end
+        if @user && !@user.activated
             flash.now[:errors] = ["Authentication mail has been sent ,please follow the link provided in the mail to complete the authentication process"]
             render :new 
         end
+    end
+
+    def require_admin
+        render plain: "Admin Priviledges required for this action",status: :forbidden unless admin?
+    end
+
+    def admin?
+        current_user.admin
     end
 end
