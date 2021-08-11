2d9feee7f0cf3051042ce1ba8d72ab4c9034e966
jimosk <jimosdk@gmail.com>
2021-08-11 17:41:47 +0300

    Added tests for  User::find_by_credentials,#is_password?,#reset_session_token!

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject(:user) {User.create(name:'John',password: '123456',email: 'john@mail')}
+  subject(:user) {User.create!(name:'John',password: '123456',email: 'john@mail')}
 
   it {should validate_presence_of(:email)}
   it {should validate_presence_of(:password_digest)}
@@ -14,4 +14,48 @@ RSpec.describe User, type: :model do
   it {should validate_uniqueness_of(:session_token)}
 
   it {should validate_length_of(:password).is_at_least(6).allow_nil}
+
+  describe "#is_password?" do
+    it "should return true if the password matches" do
+      expect(user.is_password?('123456')).to be true
+    end
+
+    it "should return false if there is a password missmatch" do
+      expect(user.is_password?('12345')).to be false
+    end
+  end
+
+  describe "#reset_session_token" do
+    it "should change the value of the session_token" do
+      token = user.session_token
+      user.reset_session_token!
+      expect(user.session_token).to_not eq(token)
+    end
+
+    it "should save the model" do
+      expect(user).to receive(:save!)
+      user.reset_session_token!
+    end
+
+    it "should return the new session_token" do
+      expect(user.reset_session_token!).to eq(user.session_token)
+    end
+  end
+
+  describe "::find_by_credentials" do
+    before(:each) { user.reload}
+    it "should return nil if the user doesn't exist in the database" do
+      expect(User.find_by_credentials('john@mai','123456')).to be_nil
+    end
+    it "should return nil if the password doesn't match" do
+      expect(User.find_by_credentials('john@mail','12345')).to be_nil
+    end
+    it "should return the user if the credentials match" do
+      user2 = User.find_by_credentials('john@mail','123456')
+      expect(user2).to be_an(User)
+      expect(user2.email).to eq('john@mail')
+      expect(user2.name).to eq('John')
+      expect(user2.is_password?('123456')).to be true
+    end
+  end 
 end
